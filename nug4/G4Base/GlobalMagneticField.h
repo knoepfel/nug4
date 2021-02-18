767cab8a4c023311f44f1702ad0587a7d0a70ca9
Eldwan Brianne <eldwan35@gmail.com>
2021-02-18 16:03:07 +0100

    Added GlobalMagneticField to handle non-uniform magnetic fields that uses the magnetic field service method FieldAtPoint(), comments and cleanup

---
+++ nug4/G4Base/GlobalMagneticField.h
@@ -0,0 +1,35 @@
+#ifndef GLOBALMAGNETICFIELD_H
+#define GLOBALMAGNETICFIELD_H
+//
+// G4 interface for full magnetic field.
+//
+//
+// Original author Eldwan Brianne 
+//
+
+#include <string>
+
+#include "Geant4/G4MagneticField.hh"
+#include "Geant4/G4Types.hh"
+#include "Geant4/G4ThreeVector.hh"
+
+namespace mag {
+
+  class MagneticFieldService;
+
+  class GlobalMagneticField: public G4MagneticField {
+
+  public:
+
+    explicit GlobalMagneticField();
+    virtual ~GlobalMagneticField(){};
+
+    // This is called by G4.
+    virtual void GetFieldValue(const G4double Point[4], G4double *Bfield) const;
+
+  private:
+
+    const mag::MagneticFieldService* _bFieldHandle;
+  };
+}
+#endif /* GLOBALMAGNETICFIELD_H */
